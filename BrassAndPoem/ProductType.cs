291d0f83005453dea9729adafae9d9d675e83413
Daniel Henderson <djhindu17@gmail.com>
2024-10-28 10:24:35 -0500

    Created ProductType Class

--- BrassAndPoem/ProductType.cs
+++ BrassAndPoem/ProductType.cs
@@ -1,1 +1,9 @@
 //create your ProductType class here
+namespace BrassAndPoem
+{
+    public class ProductType
+    {
+        public string Title { get; set; }
+        public int Id { get; set; }
+    }
+}
